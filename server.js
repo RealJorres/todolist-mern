992d15879708dc5adc087d8f439062701c327ddf
JoshuaRelatorres <relatorres_joshua@yahoo.com>
2021-07-03 04:31:40 +0800

    haha

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(express.json());
 
 //Step
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, ' frontend', 'build')));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 };
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(express.static);
 });
 
 
